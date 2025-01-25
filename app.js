e4b98877217042329b285a412e3ca9b20ef98818
Juan Sebastian Robayo Quiñones <juan.robayo.93@outlook.com>
2025-01-25 13:38:05 -0500

    1

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleButtons = document.querySelectorAll(".toggle-btn");
-  const loginForm = document.querySelector(".login");
-  const registerForm = document.querySelector(".register");
+  const loginForm = document.querySelector(".form-container.login");
+  const registerForm = document.querySelector(".form-container.register");
 
   toggleButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      loginForm.classList.toggle("active");
-      registerForm.classList.toggle("active");
+      if (loginForm.classList.contains("active")) {
+        // Si el formulario de inicio de sesión está activo, mostramos el de registro
+        loginForm.classList.remove("active");
+        registerForm.classList.add("active");
+      } else {
+        // Si el formulario de registro está activo, mostramos el de inicio de sesión
+        registerForm.classList.remove("active");
+        loginForm.classList.add("active");
+      }
     });
   });
 });
